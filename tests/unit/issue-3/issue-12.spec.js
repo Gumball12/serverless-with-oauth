9a20c605b91997d7df02b4b7d187af0b5a07fb37
HaeJun Seo <kan02134@gmail.com>
2019-10-10 23:08:40 +0900

    Chore: Create data-binding test

---
+++ tests/unit/issue-3/issue-12.spec.js
@@ -0,0 +1,35 @@
+/**
+ * testing for issue-12 / 아이디와 패스워드를 입력할 수 있는 폼 제공 <https://github.com/Gumball12/serverless-with-oauth/issues/12>
+ * @author shj
+ * @since 2019-10-10
+ */
+
+// import tetsing modules
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+
+// import target
+import Login from '@/views/Login.vue';
+
+// create test
+describe('Login.vue', () => {
+  /**
+   * binding test
+   */
+  it('data <=> DOM binding test', () => {
+    // get wrapper
+    const wrapper = shallowMount(Login);
+
+    // get target instance
+    const target = wrapper.find({ ref: 'id-input-field' });
+
+    // define test text
+    const text = 'test_id';
+
+    // update data value
+    wrapper.setData({ text });
+
+    // check target value
+    expect(target.text()).toBe(text);
+  });
+});
