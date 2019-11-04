8bab256c70d50d6f81ec5df3b723ff600cb5acfa
HaeJun Seo <kan02134@gmail.com>
2019-11-04 09:18:12 +0900

    Feat: Add router guard
    - check token
    
    Closes #32

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    Home
+    <v-btn @click="doLogout">logout</v-btn>
+  </v-container>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'home',
+  methods: {
+    /**
+     * logout action
+     */
+    doLogout() {
+      // clear all token
+      this.clearToken();
+
+      // go to the login page
+      this.$router.push('login');
+    },
+    // vuex
+    ...mapActions([
+      'clearToken',
+    ]),
+  },
+};
+</script>
